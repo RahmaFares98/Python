39c92df1dcb7e171ba9f6b1ab56c370d6704f00e
rahmafaris981 <rahmafaris981@gmail.com>
2024-05-26 23:01:00 +0300

    seccommit

--- hello_world.py
+++ hello_world.py
@@ -13,4 +13,6 @@ fave_food1 = "sushi"
 fave_food2 = "pizza"
 print("I love to eat {} and {}".format(fave_food1, fave_food2))
 print(f"I love to eat {fave_food1} a {fave_food2}")
-
+text = "Hello World"
+new_text = text.replace("World", "Universe")
+print(new_text)
