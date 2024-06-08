8aa61559046c4cea54059308b8bf28f772667ceb
rahmafaris981 <rahmafaris981@gmail.com>
2024-06-08 20:09:32 +0300

    MathDojo Assigenment

--- mathdojo.py
+++ mathdojo.py
@@ -17,13 +17,14 @@ class MathDojo:
 md = MathDojo()
 # to test:
 n = md.add(2).add(2,5,1).subtract(3,2).result
-print(n)	# should print 5
+print(f"the value of y is equal {n}")# should print 5
 # run each of the methods a few more times and check the result!
 x = md.add(10,9,3,4,2,1).result
 y=md.add(2).add(5,6,3,7,2).subtract(3,5,1,8,3).add(100).result
-print(x)
-print(y)
 y=md.add(4).subtract(4,2).result
-print(y)
-
-
+y=md.add(10).result         
+y=md.subtract(5).result
+print(f"the value of y is equal {y}")
+x=md.add(3, 4, 5).add(4,3).add(4,5,2,7,1).result    
+x=md.subtract(1, 1, 1).subtract(1,4).subtract(3).result
+print(f"the value of x is equal {x}")
